d784e052334ed8452e5252e330e83eaaf9efecf9
LSG <dltmdrb1996@naver.com>
2021-05-12 00:44:33 +0900

    유튜브 구현 완료

--- app/src/main/java/com/example/today/presentation/MusicFragment.kt
+++ app/src/main/java/com/example/today/presentation/MusicFragment.kt
@@ -1,12 +1,16 @@
 package com.example.today.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.today.databinding.FragmentMusicBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+
 
 class MusicFragment : Fragment() {
 
@@ -26,4 +30,17 @@ class MusicFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.music.observe(viewLifecycleOwner,{
+            binding.youtubePlayerView.addYouTubePlayerListener(object :
+                AbstractYouTubePlayerListener() {
+                override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
+                    val videoId = "Cvq4hflIZj0"
+                    youTubePlayer.cueVideo(videoId, 0f)
+                }
+            })
+        })
+    }
 }
